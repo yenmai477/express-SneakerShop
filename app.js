b733f2c41232d37b70d4db7325eeb1223332dd67
Yen Mai <yenmai478@gmail.com>
2019-10-24 08:30:18 +0700

    Update setting project

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
@@ -49,6 +50,8 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+app.use(compression());
 
 // 2) ROUTES
 app.use('/api/v1/users', userRouter);
