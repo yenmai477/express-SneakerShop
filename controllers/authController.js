d023d1fb4d7b9b31eeb6cd4485e7716c19817974
Yen Mai <yenmai478@gmail.com>
2019-12-07 22:43:06 +0700

    Update project

--- controllers/authController.js
+++ controllers/authController.js
@@ -124,9 +124,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/users/resetPassword/${resetToken}`;
+  const resetURL = `intent:#Intent;scheme=qssneaker://users/resetPassword/${resetToken};package=com.yenmaiteam.qssneaker;end`;
 
   try {
     // await sendEmail({
