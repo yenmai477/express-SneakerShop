a55bcb2da52fdcb709a957a02051261adf06e9ef
Yen Mai <yenmai478@gmail.com>
2019-12-08 22:01:32 +0700

    Update review routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,12 +6,11 @@ const router = express.Router({ mergeParams: true });
 
 router.get('/analysisReview/:productId', reviewController.analysisReview);
 
-router.use(authController.protect);
-
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
+    authController.protect,
     authController.restrictTo('user'),
     reviewController.setProductUserIds,
     reviewController.createReview
@@ -21,10 +20,12 @@ router
   .route('/:id')
   .get(reviewController.getReview)
   .patch(
+    authController.protect,
     authController.restrictTo('user', 'admin'),
     reviewController.updateReview
   )
   .delete(
+    authController.protect,
     authController.restrictTo('user', 'admin'),
     reviewController.checkUserDelete,
     reviewController.deleteReview
